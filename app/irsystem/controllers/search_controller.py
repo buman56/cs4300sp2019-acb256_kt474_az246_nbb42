0c1bda6eafefb60381eade9866c974ad4cf1606c
Kevin Tian <kt474@cornell.edu>
2019-04-14 17:49:52 -0400

    milestone1 updates

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,4 +1,5 @@
 from . import *  
+from app.irsystem.controllers.tfidf import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
@@ -8,13 +9,19 @@ net_id = "Kevin Tian(kt474), Alex Zhou(az246), Noah Beller(nbb42), Aaron Bu(acb2
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
+	test = get_suggestions(query)
 	if not query:
 		data = []
+		
 		output_message = ''
 	else:
-		output_message = "Your search: " + query
+		output_message = "You searched for: " + query
+		top_result = " " + str(get_suggestions(query)[0][0]) 
+		description = " " + str(get_suggestions(query)[0][2]) 
+		# other_results = " " + str(get_suggestions(query)[1][0]) 
 		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+
+	return render_template('search.html',test = test, name=project_name, netid=net_id, output_message=output_message, data=data, top_result = top_result, description = description)
 
 
 
---
+++ app/irsystem/controllers/tfidf.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import math
+from sklearn.metrics.pairwise import cosine_similarity
+
+def build_vectorizer(max_features, stop_words, max_df=0.8, min_df=10, norm='l2'):
+	return TfidfVectorizer(stop_words=stop_words,max_df=max_df, min_df=min_df, max_features=max_features, norm=norm)
+
+def get_sim(query, doc_index, doc_by_vocab):
+    return cosine_similarity(query,doc_by_vocab)
+
+def get_suggestions(q):
+	df = pd.read_csv('tripadvisor_merged.csv')
+	desc_data = []
+	# 
+	for d in df['Description']:
+		if(isinstance(d, float)):
+			if(math.isnan(d)):
+				desc_data.append('')
+		else:
+			desc_data.append(d)
+
+	movie_index_to_name = {index:movie_name for index, movie_name in enumerate(df['MuseumName'])}
+	movie_index_to_description = {index:desc for index, desc in enumerate(desc_data)}
+
+	vectorizer = build_vectorizer(5000, "english")
+	doc_by_vocab = vectorizer.fit_transform(desc_data)
+	query = vectorizer.transform([q])
+	sim = get_sim(query,5,doc_by_vocab)[0]
+	top_5_idx = np.argsort(sim)[-5:]
+	top_5 = []
+
+	for i in reversed(top_5_idx): 
+		top_5.append((movie_index_to_name[i],sim[i],movie_index_to_description[i]))
+
+	return top_5
+		
+
+def main():
+	print(get_suggestions('war memorial service'))
+
+if __name__ == "__main__":
+	main()
+
