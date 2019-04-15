46bb5e88e301d1ca813e1b613b7695f067bae271
Kevin Tian <kt474@cornell.edu>
2019-04-14 22:51:46 -0400

    changed colors

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -9,19 +9,18 @@ net_id = "Kevin Tian(kt474), Alex Zhou(az246), Noah Beller(nbb42), Aaron Bu(acb2
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
-	test = get_suggestions(query)
+	results = get_suggestions(query)
 	if not query:
 		data = []
-		
 		output_message = ''
 	else:
 		output_message = "You searched for: " + query
-		top_result = " " + str(get_suggestions(query)[0][0]) 
-		description = " " + str(get_suggestions(query)[0][2]) 
+		# top_result = " " + str(get_suggestions(query)[0][0]) 
+		# description = " " + str(get_suggestions(query)[0][2]) 
 		# other_results = " " + str(get_suggestions(query)[1][0]) 
 		data = range(5)
 
-	return render_template('search.html',test = test, name=project_name, netid=net_id, output_message=output_message, data=data, top_result = top_result, description = description)
+	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data, results = results)
 
 
 
