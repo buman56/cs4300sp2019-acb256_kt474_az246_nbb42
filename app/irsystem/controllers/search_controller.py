ffbcdc9a2c7ff58afacc596f4e9a8bdaad5aa468
Kevin Tian <kt474@cornell.edu>
2019-04-15 00:08:59 -0400

    last changes

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -9,12 +9,14 @@ net_id = "Kevin Tian(kt474), Alex Zhou(az246), Noah Beller(nbb42), Aaron Bu(acb2
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
-	results = get_suggestions(query)
+	
 	if not query:
 		data = []
 		output_message = ''
+		results = get_suggestions(query)
 	else:
 		output_message = "You searched for: " + query
+		results = get_suggestions(query)
 		# top_result = " " + str(get_suggestions(query)[0][0]) 
 		# description = " " + str(get_suggestions(query)[0][2]) 
 		# other_results = " " + str(get_suggestions(query)[1][0]) 
