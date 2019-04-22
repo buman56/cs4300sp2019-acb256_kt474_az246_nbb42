fad84c466a5b33d060f4f64cab4aa5aaf6590a47
Alex Zhou <alexzhou@dhcp-gs-5296.redrover.cornell.edu>
2019-04-22 19:29:03 -0400

    filters

--- app/irsystem/controllers/tfidf.py
+++ app/irsystem/controllers/tfidf.py
@@ -17,6 +17,7 @@ for d in df['Description']:
 			desc_data.append('')
 	else:
 		desc_data.append(d)
+pure_desc = desc_data.copy()
 
 m_index_to_name = {index:m_name for index, m_name in enumerate(df['MuseumName'])}
 m_name_to_index = dict((v,k) for k,v in m_index_to_name.items())
@@ -25,7 +26,7 @@ for m in review_museum:
 	for review in review_js[m]:
 		desc_data[ind] = desc_data[ind] + review
 
-m_index_to_description = {index:desc for index, desc in enumerate(desc_data)}
+m_index_to_description = {index:desc for index, desc in enumerate(pure_desc)}
 
 def build_vectorizer(max_features, stop_words, max_df=0.8, min_df=1, norm='l2'):
 	return TfidfVectorizer(stop_words=stop_words,max_df=max_df, min_df=min_df, max_features=max_features, norm=norm)
@@ -42,14 +43,14 @@ def get_suggestions(q):
 	top_5 = []
 
 	for i in reversed(top_5_idx): 
-		top_5.append((m_index_to_name[i],sim[i],m_index_to_description[i]))
+		if(sim[i] > 0 and m_index_to_description[i] != ''):
+			top_5.append((m_index_to_name[i],sim[i],m_index_to_description[i]))
 
 	return top_5
 		
 
 def main():
-	print(get_suggestions('war memorial service'))
-	#get_suggestions('war memorial service')
+	print(get_suggestions('sdfds'))
 
 if __name__ == "__main__":
 	main()
