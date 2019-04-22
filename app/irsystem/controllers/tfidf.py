ff62f502bbaa753bfa0fb499fbfdb274bb57a464
Aaron Bu <acb256@cornell.edu>
2019-04-22 19:35:06 -0400

    alex's error

--- app/irsystem/controllers/tfidf.py
+++ app/irsystem/controllers/tfidf.py
@@ -14,22 +14,24 @@ review_museum = (review_js.keys())
 
 desc_data = []
 for d in df['Description']:
-	if(isinstance(d, float)):
-		if(math.isnan(d)):
-			desc_data.append('')
-	else:
-		desc_data.append(d)
+    if(isinstance(d, float)):
+        if(math.isnan(d)):
+            desc_data.append('')
+    else:
+        desc_data.append(d)
 pure_desc = desc_data.copy()
 
-m_index_to_name = {index:m_name for index, m_name in enumerate(df['MuseumName'])}
-m_name_to_index = dict((v,k) for k,v in m_index_to_name.items())
+m_index_to_name = {index: m_name for index,
+                   m_name in enumerate(df['MuseumName'])}
+m_name_to_index = dict((v, k) for k, v in m_index_to_name.items())
 
 for m in review_museum:
     ind = m_name_to_index[m]
     for review in review_js[m]:
         desc_data[ind] = desc_data[ind] + review
 
-m_index_to_description = {index:desc for index, desc in enumerate(pure_desc)}
+m_index_to_description = {index: desc for index, desc in enumerate(pure_desc)}
+
 
 def build_vectorizer(max_features, stop_words, max_df=0.8, min_df=1, norm='l2'):
     return TfidfVectorizer(stop_words=stop_words, max_df=max_df, min_df=min_df, max_features=max_features, norm=norm)
@@ -47,9 +49,13 @@ def get_suggestions(q):
     top_5_idx = np.argsort(sim)[-5:]
     top_5 = []
 
-	for i in reversed(top_5_idx): 
-		if(sim[i] > 0 and m_index_to_description[i] != ''):
-			top_5.append((m_index_to_name[i],sim[i],m_index_to_description[i]))
+    for i in reversed(top_5_idx):
+        if(sim[i] > 0 and m_index_to_description[i] != ''):
+            top_5.append(
+                (m_index_to_name[i], sim[i], m_index_to_description[i]))
+
+    return top_5
+
 
 def closest_projects(docs_compressed, project_index_in, k=5):
     sims = docs_compressed.dot(docs_compressed[project_index_in, :])
@@ -70,10 +76,10 @@ def museum_match(q):
     return closest_projects(docs_compressed, m_name_to_index[q], 5)
 
 
-
 def main():
     print(get_suggestions('war memorial service'))
     print(museum_match("he"))
+
 
 if __name__ == "__main__":
     main()
