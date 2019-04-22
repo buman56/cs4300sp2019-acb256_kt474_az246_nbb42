f49f07929533e15d48b4d44794a668d629ce61a0
Kevin Tian <kt474@cornell.edu>
2019-04-22 19:50:04 -0400

    test

--- app/irsystem/controllers/tfidf.py
+++ app/irsystem/controllers/tfidf.py
@@ -14,15 +14,17 @@ review_museum = (review_js.keys())
 
 desc_data = []
 for d in df['Description']:
-    if(isinstance(d, float)):
-        if(math.isnan(d)):
+    if (isinstance(d, float)):
+        if (math.isnan(d)):
             desc_data.append('')
     else:
         desc_data.append(d)
 pure_desc = desc_data.copy()
 
-m_index_to_name = {index: m_name for index,
-                   m_name in enumerate(df['MuseumName'])}
+m_index_to_name = {
+    index: m_name
+    for index, m_name in enumerate(df['MuseumName'])
+}
 m_name_to_index = dict((v, k) for k, v in m_index_to_name.items())
 
 for m in review_museum:
@@ -33,11 +35,6 @@ for m in review_museum:
 m_index_to_description = {index: desc for index, desc in enumerate(pure_desc)}
 
 
-def build_vectorizer(max_features, stop_words, max_df=0.8, min_df=1, norm='l2'):
-    return TfidfVectorizer(stop_words=stop_words, max_df=max_df, min_df=min_df, max_features=max_features, norm=norm)
-
-<<<<<<< HEAD
-
 def build_vectorizer(max_features, stop_words, max_df=0.8, min_df=1,
                      norm='l2'):
     return TfidfVectorizer(stop_words=stop_words,
@@ -46,36 +43,12 @@ def build_vectorizer(max_features, stop_words, max_df=0.8, min_df=1,
                            max_features=max_features,
                            norm=norm)
 
-=======
->>>>>>> ff62f502bbaa753bfa0fb499fbfdb274bb57a464
 
 def get_sim(query, doc_index, doc_by_vocab):
     return cosine_similarity(query, doc_by_vocab)
 
 
 def get_suggestions(q):
-<<<<<<< HEAD
-    df = pd.read_csv('tripadvisor_merged.csv')
-    desc_data = []
-
-    for d in df['Description']:
-        if (isinstance(d, float)):
-            if (math.isnan(d)):
-                desc_data.append('')
-        else:
-            desc_data.append(d)
-
-    movie_index_to_name = {
-        index: movie_name
-        for index, movie_name in enumerate(df['MuseumName'])
-    }
-    movie_index_to_description = {
-        index: desc
-        for index, desc in enumerate(desc_data)
-    }
-
-=======
->>>>>>> ff62f502bbaa753bfa0fb499fbfdb274bb57a464
     vectorizer = build_vectorizer(5000, "english")
     doc_by_vocab = vectorizer.fit_transform(desc_data)
     query = vectorizer.transform([q])
@@ -84,17 +57,7 @@ def get_suggestions(q):
     top_5 = []
 
     for i in reversed(top_5_idx):
-<<<<<<< HEAD
-        top_5.append(
-            (movie_index_to_name[i], sim[i], movie_index_to_description[i]))
-
-    return top_5
-
-
-def main():
-    print(get_suggestions('war memorial service'))
-=======
-        if(sim[i] > 0 and m_index_to_description[i] != ''):
+        if (sim[i] > 0 and m_index_to_description[i] != ''):
             top_5.append(
                 (m_index_to_name[i], sim[i], m_index_to_description[i]))
 
@@ -103,8 +66,9 @@ def main():
 
 def closest_projects(docs_compressed, project_index_in, k=5):
     sims = docs_compressed.dot(docs_compressed[project_index_in, :])
-    asort = np.argsort(-sims)[:k+1]
-    return [(m_index_to_name[i], sims[i]/sims[asort[0]], m_index_to_description[i]) for i in asort[1:]]
+    asort = np.argsort(-sims)[:k + 1]
+    return [(m_index_to_name[i], sims[i] / sims[asort[0]],
+             m_index_to_description[i]) for i in asort[1:]]
 
 
 def museum_match(q):
@@ -123,7 +87,6 @@ def museum_match(q):
 def main():
     print(get_suggestions('war memorial service'))
     print(museum_match("he"))
->>>>>>> ff62f502bbaa753bfa0fb499fbfdb274bb57a464
 
 
 if __name__ == "__main__":
